b5f35a95ce3845ec254cb9c389e548b86106ad43
rickyaguayo <raguayo911@gmail.com>
2021-11-17 15:41:32 -0600

    first commit

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,23 @@
+let country = "USA";
+let continent = "North America";
+let population = "329.5 million";
+
+console.log(country + ', ' + continent + ', ' + population);
+
+let jsIsFun = true;
+console.log(jsIsFun);
+
+// console.log(typeof true);
+console.log(typeof jsIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'ricky');
+
+jsIsFun = 'YES!'
+console.log(typeof jsIsFun);
+
+let year;
+console.log(year)
+console.log(typeof year)
+
+year = 1991;
+console.log(typeof year)
