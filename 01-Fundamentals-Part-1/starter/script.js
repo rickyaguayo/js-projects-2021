e9935fed54bbdd65fa7fa782780929a1fd895f17
rickyaguayo <raguayo911@gmail.com>
2021-11-17 19:03:38 -0600

    added operator section

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,39 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
 let firstName = "Ricky"
+
+let age = 29;
+age = 30;
+
+const birthYear = 1991;
+birthYear = 1990; 
+
+*/
+
+// math operators
+const now = 2037;
+const ageRicky = now - 1992;
+const ageMonica = now - 1991;
+
+console.log(ageRicky, ageMonica);
+console.log(ageRicky * 2, ageRicky / 10, 2 ** 3);
+
+const firstName = 'Ricky';
+const lastName = 'Aguayo';
+
+console.log(firstName + ' ' + lastName);
+
+//assignment operators
+let x = 10 + 5;
+x += 10; // x = x + 10
+x++;
+console.log(x);
+
+// comparison operators- used to create boolean values
+console.log(ageRicky < ageMonica) // <, >, <=, >=
+console.log(ageMonica >= 18)
+
+const averageAge = (ageMonica + ageRicky) / 2;
+console.log(averageAge);
