e61f41e6e4ef95829367c023a15616b894dc993c
rickyaguayo <raguayo911@gmail.com>
2021-11-17 19:03:52 -0600

    finished coding challenge 1

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,3 +1,4 @@
+/*
 let country = "USA";
 let continent = "North America";
 let population = "329.5 million";
@@ -21,3 +22,22 @@ console.log(typeof year)
 
 year = 1991;
 console.log(typeof year)
+*/
+
+// let markHeight = 1.69;
+// let markWeight = 78;
+// let johnHeight = 1.95;
+// let johnWeight = 92;
+
+let markHeight = 1.88;
+let markWeight = 95;
+let johnHeight = 1.76;
+let johnWeight = 85;
+
+const markBMI = markWeight / markHeight ** 2;
+const johnBMI = johnWeight / johnHeight ** 2;
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markBMI, johnBMI, markHigherBMI);
+// console.log(johnBMI);
+// console.log(markHigherBMI);
