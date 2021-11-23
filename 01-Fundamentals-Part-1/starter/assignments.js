3456178cba57c2e80557549cc56538af07d79fc2
rickyaguayo <raguayo911@gmail.com>
2021-11-22 21:58:22 -0600

    coding challenge #3 added

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -23,9 +23,6 @@ console.log(typeof year)
 year = 1991;
 console.log(typeof year)
 
-*/
-
-
 let markHeight = 1.69;
 let markWeight = 78;
 let johnHeight = 1.95;
@@ -40,19 +37,17 @@ const markBMI = markWeight / markHeight ** 2;
 const johnBMI = johnWeight / johnHeight ** 2;
 const markHigherBMI = markBMI > johnBMI;
 
-console.log(markBMI, johnBMI, markHigherBMI);
+// console.log(markBMI, johnBMI, markHigherBMI);
 // console.log(johnBMI);
 // console.log(markHigherBMI);
 
-if (markBMI > johnBMI) {
-  // console.log("Mark's BMI is higher than John's!");
-  console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}!`);
-} else {
-  // console.log("John's BMI is higher than Mark's!");
-  console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}!`);
-}
-
-/*
+// if (markBMI > johnBMI) {
+//   console.log("Mark's BMI is higher than John's!");
+//   console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+// } else {
+//   console.log("John's BMI is higher than Mark's!");
+//   console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+// }
 
 const firstName = 'Ricky';
 const job = 'developer';
@@ -67,3 +62,51 @@ const rickyNew = `I'm ${firstName} a ${currentYear - birthYear} year old ${job}!
 console.log(rickyNew);
 
 */
+
+// const dolphinsAverage = (97 + 112 + 101) / 3;
+// const koalasAverage = (109 + 95 + 123) / 3;
+
+// console.log(dolphinsAverage, koalasAverage);
+
+// if (dolphinsAverage > koalasAverage) {
+//   console.log("Dolphins win!");
+// } else if (dolphinsAverage < koalasAverage) {
+//   console.log("Koalas win!");
+// } else {
+//   console.log("It's a tie!");
+// }
+
+// if (dolphinsAverage > 100 && koalasAverage > 100) {
+//   if (dolphinsAverage > koalasAverage) {
+//     console.log("Dolphins win!");
+//   } else if (dolphinsAverage < koalasAverage) {
+//     console.log("Koalas win!");
+//   } else {
+//     console.log("It's a tie!");
+//   }
+// } else if (dolphinsAverage < 100 && koalasAverage > 100) {
+//   console.log("Koalas Win! 1");
+// } else if (dolphinsAverage > 100 && koalasAverage < 100) {
+//   console.log("Dolphins Win! 2");
+// } else {
+//   console.log("Min 100 points to win");
+// }
+
+const dolphinsAverage = (97 + 112 + 101) / 3;
+const koalasAverage = (109 + 95 + 106) / 3;
+
+console.log(dolphinsAverage, koalasAverage);
+
+if (dolphinsAverage > koalasAverage && dolphinsAverage >= 100) {
+  console.log("Dolphins win!");
+} else if (dolphinsAverage < koalasAverage && koalasAverage >= 100) {
+  console.log("Koalas win!");
+} else if (
+  dolphinsAverage === koalasAverage &&
+  dolphinsAverage >= 100 &&
+  koalasAverage >= 100
+) {
+  console.log("It's a tie!");
+} else {
+  console.log("no one wins");
+}
