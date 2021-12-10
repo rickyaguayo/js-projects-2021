4003548e4cdb6e29b6824e2b4557d5ce39fe85ca
rickyaguayo <raguayo911@gmail.com>
2021-12-09 23:05:44 -0600

    random new shit

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  // ES5 way
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price
+  };
+
+  console.log(booking);
+  bookings.push(booking)
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,124 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+/*
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data)
+
+    const currency_key = Object.keys(data.currencies)[0];
+    console.log(currency_key);
+    const currency = data.currencies[currency_key].name;
+    console.log(currency)
+
+    const language_key = Object.keys(data.languages)[0];
+    const language = data.languages[language_key];
+
+    const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.official}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} million people</p>
+      <p class="country__row"><span>🗣️</span>${language}</p>
+      <p class="country__row"><span>💰</span>${currency}</p>
+    </div>
+  </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('congo');
+getCountryData('brazil');
+
+const renderCountry = function (data, className = '') {
+  const currency_key = Object.keys(data.currencies)[0];
+  console.log(currency_key);
+  const currency = data.currencies[currency_key].name;
+  console.log(currency)
+
+  const language_key = Object.keys(data.languages)[0];
+  const language = data.languages[language_key];
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.official}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} million people</p>
+      <p class="country__row"><span>🗣️</span>${language}</p>
+      <p class="country__row"><span>💰</span>${currency}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+}
+
+const getCountryAndNeighbor = function (country) {
+
+  // AJAX country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // render country 1
+    renderCountry(data);
+
+    // get neighbor country
+    const [neighbor] = data.borders;
+
+    if (!neighbor) return;
+
+    // AJAX country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    })
+  });
+};
+
+// getCountryAndNeighbor('portugal');
+getCountryAndNeighbor('usa');
+
+*/
+
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+// request.send();
+
+// const request = fetch('https://restcountries.com/v3.1/name/portugal');
+// console.log(request);
+
+const closureFunc = function () {
+  let counter = 0;
+
+  return function () {
+    counter++;
+    console.log(`${counter}`)
+  }
+}
+
+const storage = closureFunc();
+
+storage(); // 1
+storage(); // 2
+storage(); // 3
